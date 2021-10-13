9e5631c70ddd85cf24a504d83b50e74a9771f2de
Moritz Obermeier <mo@bluebirdmountain.de>
2021-10-13 21:21:05 +0200

    prettier

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,13 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  "appId": "de.moritzobermeier.meditationTimer",
-  "appName": "meditation-timer",
-  "bundledWebRuntime": false,
-  "npmClient": "npm",
-  "webDir": "www",
-  "cordova": {},
-  "linuxAndroidStudioPath": "/home/mo/tools/android-studio/bin/studio.sh"
+  appId: "de.moritzobermeier.meditationTimer",
+  appName: "meditation-timer",
+  bundledWebRuntime: false,
+  npmClient: "npm",
+  webDir: "www",
+  cordova: {},
+  linuxAndroidStudioPath: "/home/mo/tools/android-studio/bin/studio.sh"
 }
 
 export default config;
