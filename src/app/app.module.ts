b7bde0a23c24df8f6fedcf287340f957d02b2482
Moritz Obermeier <2733691+moobsen@users.noreply.github.com>
2023-12-01 22:31:45 +0100

    more updates and reverting "unused" changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +15,8 @@ import { Insomnia } from '@ionic-native/insomnia/ngx';
     declarations: [AppComponent],
     imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
     providers: [
+        StatusBar,
+        SplashScreen,
         NativeAudio,
         Insomnia,
         { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
