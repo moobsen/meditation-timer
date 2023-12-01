bab6cebb853e86865c2967270eed2c8f74e7945c
Moritz Obermeier <mo@bluebirdmountain.de>
2023-12-01 09:28:19 +0100

    updating ng, removing unsed ionic-native stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,8 +13,6 @@ import { Insomnia } from '@ionic-native/insomnia/ngx';
     declarations: [AppComponent],
     imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
     providers: [
-        StatusBar,
-        SplashScreen,
         NativeAudio,
         Insomnia,
         { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
